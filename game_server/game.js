509237cbeb8b7ba07ee349c5a505b1894e64b49a
netaro-N <takebbb1+github@gmail.com>
2019-04-18 11:45:35 +0000

    12-1

--- game_server/game.js
+++ game_server/game.js
@@ -74,7 +74,49 @@ function decreaseAir(playerObj){
 }
 
 function checkGetItem(playersMap, itemsMap, airMap) {
-    
+    for (let [hashKey, playerObj] of playersMap) {
+        if (playerObj.isAlive === false) continue;
+
+        // アイテムのミサイル（赤丸）
+        for (let [itemKey, itemObj] of itemsMap) {
+
+            const distanceObj = calculationBetweenTwoPoints(
+                playerObj.x, playerObj.y, itemObj.x, itemObj.y, gameObj.fieldWidth, gameObj.fieldHeight
+            );
+
+            if (
+                distanceObj.distanceX <= (gameObj.submarineImageWidth / 2 + gameObj.itemRadius) &&
+                distanceObj.distanceY <= (gameObj.submarineImageWidth / 2 + gameObj.itemRadius) 
+            ) {  // got item!
+
+                gameObj.itemsMap.delete(itemKey);
+                playerObj.missilesMany = playerObj.missilesMany > 5 ? 6 : playerObj.missilesMany + 1;
+                addItem();
+            }
+        }
+
+        // アイテムの空気（青丸）
+        for (let [airKey, airObj] of airMap) {
+
+            const distanceObj = calculationBetweenTwoPoints(
+                playerObj.x, playerObj.y, airObj.x, airObj.y, gameObj.fieldWidth, gameObj.fieldHeight
+            );
+
+            if (
+                distanceObj.distanceX <= (gameObj.submarineImageWidth / 2 + gameObj.airRadius) &&
+                distanceObj.distanceY <= (gameObj.submarineImageWidth / 2 + gameObj.airRadius) 
+            ) {  // got air!
+
+                gameObj.airMap.delete(airKey);
+                if (playerObj.airTime + gameObj.addAirTime > 99) {
+                    playerObj.airTime = 99;
+                } else {
+                    playerObj.airTime += gameObj.addAirTime;
+                }
+                addAir();
+            }
+        }
+    }
 }
 
 function newConnection(socketId, displayName, thumbUrl) {
@@ -90,6 +132,9 @@ function newConnection(socketId, displayName, thumbUrl) {
         thumbUrl: thumbUrl,
         isAlive: true,
         direction: 'right',
+        missilesMany: 0,
+        airTime: 99,
+        aliveTime: { 'clock': 0, 'seconds':0 },
         score: 0
     };
     gameObj.playersMap.set(socketId, playerObj);
@@ -117,6 +162,8 @@ function getMapData() {
         playerDataForSend.push(plyer.score);
         playerDataForSend.push(plyer.isAlive);
         playerDataForSend.push(plyer.direction);
+        playerDataForSend.push(plyer.missilesMany);
+        playerDataForSend.push(plyer.airTime);
 
         playersArray.push(playerDataForSend);
     }
@@ -182,6 +229,54 @@ function addAir() {
     };
     gameObj.airMap.set(airKey, airObj);
 }
+
+function calculationBetweenTwoPoints(pX, pY, oX, oY, gameWidth, gameHeight) {
+    let distanceX = 99999999;
+    let distanceY = 99999999;
+  
+    if (pX <= oX) {
+        // 右から
+        distanceX = oX - pX;
+        // 左から
+        let tmpDistance = pX + gameWidth - oX;
+        if (distanceX > tmpDistance) {
+            distanceX = tmpDistance;
+        }
+  
+    } else {
+        // 右から
+        distanceX = pX - oX;
+        // 左から
+        let tmpDistance = oX + gameWidth - pX;
+        if (distanceX > tmpDistance) {
+            distanceX = tmpDistance;
+        }
+    }
+  
+    if (pY <= oY) {
+        // 下から
+        distanceY = oY - pY;
+        // 上から
+        let tmpDistance = pY + gameHeight - oY;
+        if (distanceY > tmpDistance) {
+            distanceY = tmpDistance;
+        }
+  
+    } else {
+        // 上から
+        distanceY = pY - oY;
+        // 下から
+        let tmpDistance = oY + gameHeight - pY;
+        if (distanceY > tmpDistance) {
+            distanceY = tmpDistance;
+        }
+    }
+  
+    return {
+        distanceX,
+        distanceY
+    };
+  }
 
 module.exports = {
   newConnection,
